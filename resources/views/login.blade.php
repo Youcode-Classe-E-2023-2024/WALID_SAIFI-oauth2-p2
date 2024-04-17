9f9ab26a47b674985912e877e15fd104357fc4bc
Walid Saifi <saifiwalid21@gmail.com>
2024-04-17 09:22:30 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,8 @@
                     <h2 class="mb-0">Connexion</h2>
                 </div>
                 <div class="card-body">
-                    <form id="loginForm" method="post" action="">
+                    <form id="loginForm" method="post" action="/login">
+                        @csrf <!-- CSRF Token -->
                         <div class="form-group mb-3">
                             <label for="email">Email :</label>
                             <input name="email" type="email" class="form-control" id="email" placeholder="Entrez votre email">
@@ -40,23 +41,22 @@
 </div>
 
 <script>
+    console.log('testing')
     document.getElementById('loginForm').addEventListener('submit', function(event) {
         event.preventDefault();
 
         var formData = new FormData(this);
-        fetch('/login', {
+        fetch('api/login', {
             method: 'POST',
             body: formData
         })
             .then(response => {
-                if (!response.ok) {
-                    throw new Error('La réponse du réseau n\'est pas valide');
-                }
                 return response.json();
             })
             .then(data => {
                 // Gérer la connexion réussie
                 console.log(data);
+                localStorage.setItem('token', data.data.token)
                 window.location.href = '/home';
 
             })
